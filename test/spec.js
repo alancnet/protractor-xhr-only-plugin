ea6468c4c8bf229b437dd9e0d1de14e301107f38
Michael Giambalvo <heathkit@google.com>
2016-03-07 19:05:12 -0800

    Clean up tests slightly.

--- test/spec.js
+++ test/spec.js
@@ -3,19 +3,16 @@ describe('exchange rate tool', function() {
     browser.get('http://localhost:3456');
   });
 
-  it('should be able to wait only for $http, not $timeout', function() {
-    var loadButton = element(by.id('load-button'));
-    var loadStatus = element(by.id('load-status'));
+  var loadButton = element(by.id('load-button'));
+  var loadStatus = element(by.id('load-status'));
 
+  it('should be able to wait only for $http, not $timeout', function() {
     protractor.waitForXHROnly(true);
     loadButton.click();
     expect(loadStatus.getText()).toEqual('Waiting on timeout');
   });
 
   it('should wait for $timeout by default', function() {
-    var loadButton = element(by.id('load-button'));
-    var loadStatus = element(by.id('load-status'));
-
     protractor.waitForXHROnly(false);
     loadButton.click();
     expect(loadStatus.getText()).toEqual('Timeout done.');
